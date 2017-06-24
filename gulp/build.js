ed8e41211065d0ba821bfa2d62b1386685aa52f1
Bharath V <bharathkumar.v@nielsen.com>
2017-06-24 17:12:04 +0530

    Initial Commit

---
+++ gulp/build.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var config = require('./config');
+
+var $ = require('gulp-load-plugins')();
+var gulp = require('gulp');
+var path = require('path');
+
+gulp.task('build', ['html', 'fonts', 'other']);
+
+gulp.task('html', ['inject', 'partials'], function() {
+  var partialsInjectFile = gulp.src(
+      path.join(config.paths.tmp, '/partials/templateCacheHtml.js'),
+      {read: false});
+  var partialsInjectOptions = {
+    addRootSlash: false,
+    ignorePath: path.join(config.paths.tmp, '/partials'),
+    starttag: '<!-- inject:partials -->',
+  };
+
+  var cssFilter = $.filter('**/*.css', {restore: true});
+  var jsFilter = $.filter('**/*.js', {restore: true});
+  var htmlFilter = $.filter('*.html', {restore: true});
+
+  return gulp.src(path.join(config.paths.tmp, '/serve/*.html')).
+      pipe($.inject(partialsInjectFile, partialsInjectOptions)).
+      pipe($.useref()).
+      pipe(jsFilter).
+      pipe($.rev()).
+      pipe($.sourcemaps.init()).
+      pipe($.ngAnnotate()).
+      pipe($.uglify({preserveComments: 'license'})).
+      on('error', config.errorHandler('Uglify')).
+      pipe($.sourcemaps.write('maps')).
+      pipe(jsFilter.restore).
+      pipe(cssFilter).
+      pipe($.rev()).
+      pipe($.sourcemaps.init()).
+      pipe($.cssnano({zindex: false})).
+      pipe($.sourcemaps.write('maps')).
+      pipe(cssFilter.restore).
+      pipe($.revReplace()).
+      pipe(htmlFilter).
+      pipe($.htmlmin(config.htmlminOptions)).
+      pipe(htmlFilter.restore).
+      pipe(gulp.dest(path.join(config.paths.dist, '/'))).
+      pipe($.size({title: path.join(config.paths.dist, '/'), showFiles: true}));
+});
---
+++ src/app/home/home.component.js
@@ -0,0 +1,30 @@
+(function() {
+  'use strict';
+
+  angular.module('app').component('home', {
+    controller: HomeController,
+    controllerAs: 'vm',
+    templateUrl: 'app/home/home.view.html',
+  });
+
+  /** @ngInject */
+  function HomeController($log, SAMPLE_CONSTANT) {
+    var vm = this;
+
+    vm.greeting = '';
+
+    vm.showSampleConstant = showSampleConstant;
+
+    activate();
+
+    function activate() {
+      vm.greeting = 'Hello, world god!';
+      $log.debug('home activation');
+    }
+
+    function showSampleConstant() {
+      alert(SAMPLE_CONSTANT);
+    }
+  }
+
+})();
